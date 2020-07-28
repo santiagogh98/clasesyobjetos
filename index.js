e6d49ab6e6397cc78ae494321ff279cbabf2ef32
Sergio Santiago Garcìa Huazo <sergiosantiago@MacBook-Air-de-Sergio.local>
2020-07-28 16:41:47 -0500

    Prueba

--- index.js
+++ index.js
@@ -1,82 +1,86 @@
 
-// //OBJETOS LITERALES
-let persona ={
+// // //OBJETOS LITERALES
+// let persona ={
 
-    //Atributos
-    //LLAVE:VALOR,
-    nombre:'Mauricio',
-    apellido:'Ochoa',
-    edad:21,
-    país:'México',
-    esMayor: true,
-
-
-    //METODOS
-    saludar(){
-        console.log('Hola este es un metodo')
-    },
-
-    despedir(){
-        console.log('Adios')
-    },
-    gritar:()=> {
-        console.log('Estoy gritando')
-    },
-    caminar: function(){
-        console.log('Estoy caminando')
-    }
-};
-// // console.log(persona);
-// // console.log(persona.nombre);
-// // console.log(persona.apellido);
-// // console.log(persona.edad);
-// // console.log(persona.país);
-
-// // persona.saludar();
-// // persona.despedir();
-// // persona.gritar();
-// // persona.caminar();
-
-// let perrito={
 //     //Atributos
-//     color:'Gris',
-//     patas:3,
-//     tienePelo:false,
-//     nombre:'Conchita',
+//     //LLAVE:VALOR,
+//     nombre:'Mauricio',
+//     apellido:'Ochoa',
+//     edad:21,
+//     país:'México',
+//     esMayor: true,
 
-//     //Metodos
-//     ladrar:function(){
-//         console.log('Guaaaauuu');
+
+//     //METODOS
+//     saludar(){
+//         console.log('Hola este es un metodo')
 //     },
-//     comer:function(){
-//         console.log('Estoy comiendo')
+
+//     despedir(){
+//         console.log('Adios')
+//     },
+//     gritar:()=> {
+//         console.log('Estoy gritando')
+//     },
+//     caminar: function(){
+//         console.log('Estoy caminando')
 //     }
+// };
+// // // console.log(persona);
+// // // console.log(persona.nombre);
+// // // console.log(persona.apellido);
+// // // console.log(persona.edad);
+// // // console.log(persona.país);
 
-// }
-// console.log(perrito);
-// perrito.dueno='Hiromi';
-// console.log(perrito);
+// // // persona.saludar();
+// // // persona.despedir();
+// // // persona.gritar();
+// // // persona.caminar();
 
-// perrito.estarAlerta = function(){
-//     console.log('Te estoy vigilando');
-// }
-// console.log(perrito);
-let persona2={
-    nombre:'Hector',
-    apellido:'Lopez',
-    edad:15,
-    país:'argentina',
-    esMayor:false,
-    saludar(){
-        console.log('Hola');
+// // let perrito={
+// //     //Atributos
+// //     color:'Gris',
+// //     patas:3,
+// //     tienePelo:false,
+// //     nombre:'Conchita',
 
-    },
-    despedir(){
-        console.log('Adios');
-    }
-};
-console.log(persona);
-console.log(persona2);
+// //     //Metodos
+// //     ladrar:function(){
+// //         console.log('Guaaaauuu');
+// //     },
+// //     comer:function(){
+// //         console.log('Estoy comiendo')
+// //     }
+
+// // }
+// // console.log(perrito);
+// // perrito.dueno='Hiromi';
+// // console.log(perrito);
+
+// // perrito.estarAlerta = function(){
+// //     console.log('Te estoy vigilando');
+// // }
+// // console.log(perrito);
+// let persona2={
+//     nombre:'Hector',
+//     apellido:'Lopez',
+//     edad:15,
+//     país:'argentina',
+//     esMayor:false,
+//     saludar(){
+//         console.log('Hola');
+
+//     },
+//     despedir(){
+//         console.log('Adios');
+//     }
+// };
+// console.log(persona);
+// console.log(persona2);
 
 //CLASES
+
+console.log('hola');
+
+
 
